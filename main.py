88a7edb31e8c2c9e02fb21895622d112aed62df8
Steven HH Chen <s335276@gmail.com>
2020-12-23 18:37:32 +0800

    feat: add UI control logic with Python

--- main.py
+++ main.py
@@ -1,48 +1,63 @@
 # Python module import
 import sys
 
-# Project module imoprt
-from utils import dataset_op
-from utils.constants import *
-from utils import model_op
-
 # PyQt5 import
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.uic import loadUi
 
 # Tensorflow import
 
+# Project module imoprt
+from Resnet50 import resnet50
+
+
 class cvdl_hw01_dl_window(QMainWindow):
 
     def __init__(self):
         super(cvdl_hw01_dl_window, self).__init__()
-        loadUi('dl_select.ui', self)
+        loadUi('cvdl_2020_hw02.ui', self)
 
         # Add callback to each button
-        self.pushButton.clicked.connect(self.__butt_cb_show_train_img)
-        self.pushButton_2.clicked.connect(self.__butt_cb_show_hyper_param)
-        self.pushButton_3.clicked.connect(self.__butt_cb_show_model_struct)
-        self.pushButton_4.clicked.connect(self.__butt_cb_show_inference_acc)
-        self.pushButton_5.clicked.connect(self.__butt_cb_inference)
+        self.pushButton.clicked.connect(self.__butt_cb_1_1_bg_subtraction)
+        self.pushButton_2.clicked.connect(self.__butt_cb_2_1_optical_flow_preprocessing)
+        self.pushButton_3.clicked.connect(self.__butt_cb_2_2_optical_flow_video_tracking)
+        self.pushButton_4.clicked.connect(self.__butt_cb_3_1_perspective_transform)
+        self.pushButton_5.clicked.connect(self.__butt_cb_4_1_PCA_image_reconstruction)
+        self.pushButton_6.clicked.connect(self.__butt_cb_4_2_PCA_reconstruction_error_compute)
+        self.pushButton_7.clicked.connect(self.__butt_cb_5_1_Resnet50_show_accuracy)
+        self.pushButton_8.clicked.connect(self.__butt_cb_5_2_Resnet50_show_tensorboard_training)
+        self.pushButton_9.clicked.connect(self.__butt_cb_5_3_Resnet50_random_select_testing_image)
+        self.pushButton_10.clicked.connect(self.__butt_cb_5_4_Resnet50_show_tensorboard_training)
 
-    def __butt_cb_show_train_img(self):
-        print("Randomly pick 10 images in training set and show its label...")
-        dataset_op.cifar10_img_show()
+    def __butt_cb_1_1_bg_subtraction(self):
+        print("1.1")
 
-    def __butt_cb_show_hyper_param(self):
-        print("Hyperparameter:")
-        print("Batch size: %d" % TRAIN_BATCH_SIZE)
-        print("Learning Rate: %f" % LEARNING_RATE)
-        print("Optimizer: %s" % OPTIMIZER)
+    def __butt_cb_2_1_optical_flow_preprocessing(self):
+        print("2.1")
 
-    def __butt_cb_show_model_struct(self):
-        model_op.show_struct()
+    def __butt_cb_2_2_optical_flow_video_tracking(self):
+        print("2.2")
 
-    def __butt_cb_show_inference_acc(self):
-        model_op.show_acc()
+    def __butt_cb_3_1_perspective_transform(self):
+        print("3.1")
 
-    def __butt_cb_inference(self):
-        model_op.do_inference(self.spinBox.value())
+    def __butt_cb_4_1_PCA_image_reconstruction(self):
+        print("4.1")
+
+    def __butt_cb_4_2_PCA_reconstruction_error_compute(self):
+        print("4.2")
+
+    def __butt_cb_5_1_Resnet50_show_accuracy(self):
+        print("5.1")
+
+    def __butt_cb_5_2_Resnet50_show_tensorboard_training(self):
+        print("5.2")
+
+    def __butt_cb_5_3_Resnet50_random_select_testing_image(self):
+        print("5.3")
+
+    def __butt_cb_5_4_Resnet50_show_tensorboard_training(self):
+        print("5.4")
 
 if __name__=='__main__':
     app = QApplication(sys.argv)
